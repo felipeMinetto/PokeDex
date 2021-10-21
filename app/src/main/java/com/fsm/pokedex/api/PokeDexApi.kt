0244a41b094d6206d64f330898e8bda21f57b9c3
Felipe <felipe_minetto@bitbucket.org>
2021-10-21 17:44:25 -0300

    adding PokemonResponse.kt

--- app/src/main/java/com/fsm/pokedex/api/PokeDexApi.kt
+++ app/src/main/java/com/fsm/pokedex/api/PokeDexApi.kt
@@ -1,15 +1,16 @@
 package com.fsm.pokedex.api
 
 import com.fsm.pokedex.data.model.Pokemon
+import com.fsm.pokedex.data.model.PokemonResponse
 import retrofit2.http.GET
 
 interface PokeDexApi {
     @GET("pokemon")
-    fun getAllPokemons(): List<Pokemon>
+    fun getAllPokemons(): PokemonResponse
 
     @GET("pokemon/{id}")
-    fun getPokemon(id: Int): List<Pokemon>
+    fun getPokemon(id: Int): Pokemon
 
     @GET("pokemon/{name}")
-    fun getPokemon(name: String): List<Pokemon>
+    fun getPokemon(name: String): Pokemon
 }
