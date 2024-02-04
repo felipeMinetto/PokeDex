ec2a139225b1bea46e0f4880437c93d94ecb6e73
Felipe Minetto <fsminetto@gmail.com>
2024-02-03 18:02:26 -0600

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("kotlin-kapt")
+    id("com.google.devtools.ksp")
     id("dagger.hilt.android.plugin")
 }
 
@@ -60,19 +61,19 @@ android {
 }
 
 dependencies {
-    val navigation = "2.3.5"
-    val room = "2.3.0"
-    val lifecycle = "2.4.0"
-    val hilt = "2.39.1"
-    val moshi = "1.12.0"
+    val navigation = "2.7.6"
+    val room = "2.6.1"
+    val lifecycle = "2.7.0"
+    val hilt = "2.50"
+    val moshi = "1.15.1"
     val retrofit = "2.9.0"
-    val paging = "3.0.1"
-    val coroutines = " 1.5.2"
+    val paging = "3.2.1"
+    val coroutines = "1.7.3"
 
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.5.0")
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle")
@@ -83,7 +84,7 @@ dependencies {
 
     //Hilt
     implementation("com.google.dagger:hilt-android:$hilt")
-    kapt("com.google.dagger:hilt-compiler:$hilt")
+    ksp("com.google.dagger:hilt-compiler:$hilt")
 
     //Room
 //    implementation("androidx.room:room-runtime:$room")
@@ -102,25 +103,21 @@ dependencies {
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:$retrofit")
     implementation("com.squareup.retrofit2:converter-moshi:$retrofit")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
 
     //Paging
-    implementation("androidx.paging:paging-runtime:$paging")
+    implementation("androidx.paging:paging-runtime-ktx:$paging")
 
     //Moshi
     implementation("com.squareup.moshi:moshi:$moshi")
     implementation("com.squareup.moshi:moshi:$moshi")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshi")
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:$moshi")
 
     //Glide
-    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-}
-
-kapt {
-    correctErrorTypes = true
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+}
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43")
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
 extra["minSdkVersion"] = 21
-extra["targetSdkVersion"] = 31
+extra["targetSdkVersion"] = 34
 
 tasks.create("clean", Delete::class) {
     delete(rootProject.buildDir)
